9670367bb4da252249773e2f57e3beddcf6fab98
youngsxz8929 <youngsxz8929@ea102g1.asia-east1-b.c.ggyy-284304.internal>
2020-09-14 16:22:38 +0800

    手機

--- EA102G1/src/com/vender/model/VenderJDBCDAO.java
+++ EA102G1/src/com/vender/model/VenderJDBCDAO.java
@@ -16,19 +16,16 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+
 import java.util.ArrayList;
-import java.util.Iterator;
+
 
 public class VenderJDBCDAO implements VenderDAO_interface{
 	
